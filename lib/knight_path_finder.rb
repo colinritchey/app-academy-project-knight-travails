5ff9e22d6467631d9100ac115ac29fd930aa8bdb
App Academy Student <student@appacademy.io>
2017-01-27 15:22:33 -0800

    expanded upon build_move tree; TODO: test @move_tree

--- lib/knight_path_finder.rb
+++ lib/knight_path_finder.rb
@@ -1,6 +1,8 @@
 require_relative '00_tree_node.rb'
 
 class KnightPathFinder
+  attr_reader :move_tree
+
   DELTA = [
     [1, 2],
     [-1, 2],
@@ -11,14 +13,27 @@ class KnightPathFinder
     [-2, -1],
     [2,-1]
   ]
+
   def initialize(initial_pos = [0,0])
-    @pos = initial_pos
+    @initial_pos = initial_pos
     @move_tree = build_move_tree
     @visited_positions = [initial_pos]
   end
 
   def build_move_tree
-    root = PolyTreeNode.new(@pos)
+    root = PolyTreeNode.new(@initial_pos)
+    queue = [root]
+
+    until queue.empty?
+      current_node = queue.shift
+
+      @visited_positions << current_node.value
+      possible_moves = new_move_positions(root.value)
+
+      possible_moves.each do |pos|
+        queue << PolyTreeNode.new(pos)
+      end
+    end
 
     root
   end
@@ -37,3 +52,6 @@ class KnightPathFinder
   def find_path
   end
 end
+
+knight = KnightPathFinder.new([0,0])
+knight.move_tree
